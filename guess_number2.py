d135d032fb9a516b20a7a49f31e5b7e4f3656d17
Alsalt Alkharosi <alsaltalkharosi7@gmail.com>
2020-11-25 07:51:22 +0400

    Create guess_number2.py

---
+++ guess_number2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+Written by Alsalt Alkharosi.
+
+import random
+
+num1 = int(input('Enter your first number:'))
+num2 = int(input('Enter your last number:'))
+def range():
+    r = random.randint(num1,num2)
+    counter = 0
+    valid = False
+    while not valid:
+        while counter < 3:
+            guess = int(input('Guess the right number:'))
+            if r == guess:
+                print('[*] Well done, your guess',r,'is correct')
+                valid = True
+                exit(1)
+            elif r < guess:
+                print('[-] Your guess is higher, try something smaller!')
+                valid = False
+                counter +=1
+            elif r > guess:
+                print('[-] Your guess is smaller, try something higher')
+                valid =False
+                counter +=1
+        if counter == 3:
+            print('[!] 3 attempts failed, you have been locked out')
+            exit(1)
+        else:
+            pass
+if __name__=='__main__':
+    range()
